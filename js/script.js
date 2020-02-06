ac29c20082729a994151452e5cae13fcc4a1bfd5
Vasiliy Ivanov <vasilisko@gmail.com>
2020-02-06 16:54:28 +0300

    рефакторинг кода

--- js/script.js
+++ js/script.js
@@ -1,23 +1,12 @@
-console.log('123');
-console.warn('123');
-
-$(document).ready(function () {
-
-  $('#email').on('input', function () {
-    console.log($(this).val());
-  });
-
-
-});
-
-$(function()
-{
-  $('#email').keyup(function()
-  {
-
-    var email=$(this).val();
-    if (email.length > 3) {
-      alert('соси жепу');
+$(document).ready(function() {
+  // Комментарий надо написать
+  $('#email').on('keyup', function() {
+    var emailInput = $(this),
+        emailValue = emailInput.val();
+    if (emailValue.length > 3) {
+      emailInput.addClass('valid').removeClass('invalid');
+    } else {
+      emailInput.addClass('invalid').removeClass('valid');
     }
-  })
-})
+  });
+});
