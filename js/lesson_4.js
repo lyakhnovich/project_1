b1fb55e9422765ef9ca10fcdd8603848c86a0a61
lyakhnovich <lyakhnovich.german@gmail.com>
2020-05-19 04:09:37 +0300

    itvdn - ESSENTIAL

--- js/lesson_4.js
+++ js/lesson_4.js
@@ -1,54 +1,92 @@
 
+//
+// function Add(x, y) {
+//   var result = +x + +y;
+//   document.write(result + '</br>');
+// };
+//
+// function Sub(x, y) {
+//   var result = x - y;
+//   document.write(result + '</br>');
+// };
+//
+// function Mul(x, y) {
+//   var result = x * y;
+//   document.write(result + '</br>');
+// };
+//
+// function Div(x, y) {
+//   var result = x / y;
+//   if (y == 0){
+//     document.write('попытка деления на 0!');
+//     return;
+//   }
+//   document.write(result + '</br>');
+// };
+//
+// var x = prompt('введите операнд x'),
+//   opr = prompt('введите арифметическую опрацию (+ - * /)'),
+//   y = prompt('введите операнд y');
+//
+//
+// if(opr == '+') {
+//   document.write(x + ' + ' + y + ' = ');
+//   Add(x, y);
+//   document.write('</br>');
+// };
+//
+// if(opr == '-') {
+//   document.write(x + ' - ' + y + ' = ');
+//   Sub(x, y);
+//   document.write('</br>');
+// };
+//
+// if(opr == '*') {
+//   document.write(x + ' * ' + y + ' = ');
+//   Mul(x, y);
+//   document.write('</br>');
+// };
+//
+// if(opr == '/') {
+//   document.write(x + ' / ' + y + ' = ');
+//   Div(x, y);
+//   document.write('</br>');
+// };
 
-function Add(x, y) {
-  var result = +x + +y;
-  document.write(result + '</br>');
-};
 
-function Sub(x, y) {
-  var result = x - y;
-  document.write(result + '</br>');
-};
+function gadalka(x) {
 
-function Mul(x, y) {
-  var result = x * y;
-  document.write(result + '</br>');
-};
-
-function Div(x, y) {
-  var result = x / y;
-  if (y == 0){
-    document.write('попытка деления на 0!');
-    return;
+  if (x > 0) {
+    document.write(x + ' - положительное</br>');
+  } else if (x == 0) {
+    document.write('це нулик =)</br>');
+  } else {
+    document.write(x + ' - отрицательное</br>');
   }
-  document.write(result + '</br>');
-};
 
-var x = prompt('введите операнд x'),
-  opr = prompt('введите арифметическую опрацию (+ - * /)'),
-  y = prompt('введите операнд y');
+  var result = true;
+  for (var i = 2; i < x; i++) {
+    var number = 0;
 
+    if (x % i == 0) {
+      result = false;
+      break;
+    }
+  }
 
-if(opr == '+') {
-  document.write(x + ' + ' + y + ' = ');
-  Add(x, y);
-  document.write('</br>');
-};
+  if (x > 1 && result) {
+    number = x + ' - простое число</br>';
+  } else {
+    number = x + ' - не является простым числом</br>';
+  }
+  document.write(number);
 
-if(opr == '-') {
-  document.write(x + ' - ' + y + ' = ');
-  Sub(x, y);
-  document.write('</br>');
-};
+  if(x % 2 == 0 || x % 5 == 0 || x % 3 == 0 || x % 6 == 0 || x % 9 == 0) {
+    document.write('число ' + x + ' - делится на 2 или 5 или 3 или 6 или 9 без остатка');
+  } else {
+    document.write('число ' + x + ' - НЕ делится на 2 или 5 или 3 или 6 или 9 без остатка');
+  }
+}
 
-if(opr == '*') {
-  document.write(x + ' * ' + y + ' = ');
-  Mul(x, y);
-  document.write('</br>');
-};
-
-if(opr == '/') {
-  document.write(x + ' / ' + y + ' = ');
-  Div(x, y);
-  document.write('</br>');
-};
+var x = prompt('введите число', '0');
+gadalka(x);
