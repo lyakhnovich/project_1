db8d72bd3761853303d9515c920ba9555fdc9702
Lyakhnovich German <lyakhnovich.german@gmail.com>
2020-07-09 17:49:12 +0300

    itvdn - ADVANCED - lesson 1

--- js/advanced_les_1.js
+++ js/advanced_les_1.js
@@ -19,7 +19,42 @@ document.write(objectStr.customProperty + "<br/>");
 
 //добавить свойство в переменную simpleStr невозможно
 simpleStr.customProperty2 = 123;
-document.write(simpleStr.customProperty2);
+//document.write(simpleStr.customProperty2);
 
 
-//Конструкторы и прототипы
+//////////////////////////////Конструкторы и прототипы///////////////////////////х////у/////й
+
+//Конструктор Object()
+document.write('<hr/>');
+
+var point = new Object();
+
+point.x = 10;
+point.y = 15;
+
+document.write('Значение свойства x: ' + point.x + "<br/>");
+document.write('Значение свойства y: ' + point.y + "<br/>");
+
+//Конструктор Date()
+document.write('<hr/>');
+
+var myDate = new Date();
+
+document.write('Текущая дата и время: ' + myDate + "<br/>");
+
+//Конструктор Array()
+document.write('<hr/>');
+
+var a = Array();
+
+var b = Array(1, 2, 3, 5, 7);
+
+document.write(b + '<br/>');
+
+var c = Array(10);
+document.write(c + '<br/>');
+
+
+
+//Конструктор Object()
+document.write('<hr/>');
