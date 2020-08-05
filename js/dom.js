9748a49d83435a3b254a6ca7f2ad40e409dd5a1a
Lyakhnovich German <lyakhnovich.german@gmail.com>
2020-08-05 16:58:31 +0300

    itvdn - ADVANCED - lesson 2 - DOM

--- js/dom.js
+++ js/dom.js
@@ -19,9 +19,25 @@
 
 window.onload = function () {
 
+// -------------- childNodes
   var e = document.getElementById("list");
   // childNodes - получение всех дочерних узлов
   for (var i = 0; i < e.childNodes.length; ++i) {
     e.childNodes[i].style.color = "red";
+    alert(e.childNodes[i].nodeType); // Element
   }
+
+// -------------- childNodes 2
+  var elem = document.getElementById("list_2");
+
+  for (var i = 0; i < elem.childNodes.length; ++i) {
+    var temp = elem.childNodes[i];
+
+    // если найденный узел не элемент, то пропустить его
+    if(temp.nodeType != 1) {
+      continue;
+    }
+    elem.childNodes[i].style.color = "yellow";
+  }
+
 }
