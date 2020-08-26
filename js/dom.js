105acd096e00a2eaccaef8bca8b991719890b456
Lyakhnovich German <lyakhnovich.german@gmail.com>
2020-08-26 17:47:35 +0300

    itvdn - ADVANCED - lesson 2 - DOM - with error

--- js/dom.js
+++ js/dom.js
@@ -74,135 +74,156 @@ window.onload = function () {
 //     p2.innerHTML = "Paragraph 2";
 //     document.body.insertBefore(p2, div1);
 
+//
+//   document.getElementById("SelectFirstChild").onclick = function () {
+//     var list = document.getElementById("list_5");
+//     // Получение первого дочернего элемента в списке.
+//     var child = list.firstChild;
+//       if (child != null) {
+//         var texColor = child.style.color ? child.style.color : 'black';
+//         // добавление атрибута style="color:red" для выбранного элемента.
+//         //  child.setAttribute("style", "color:black;");
+//
+//          //alert(texColor);
+//         switch (texColor) {
+//           case "black": child.setAttribute("style", "color:purple;");
+//             break;
+//           case "purple": child.setAttribute("style", "color:blue;");
+//             break;
+//           case "blue": child.setAttribute("style", "color:green;");
+//             break;
+//           case "green": child.setAttribute("style", "color:red;");
+//             break;
+//           case "red": child.setAttribute("style", "color:yellow;");
+//             break;
+//           case "yellow": child.setAttribute("style", "color:grey;");
+//             break;
+//           case "grey": child.setAttribute("style", "color:black;");
+//             break;
+//         }
+//
+//     }
+//   };
+//
+//   document.getElementById("SelectLastChild").onclick = function () {
+//     var list = document.getElementById("list_5");
+//     // Получение первого дочернего элемента в списке.
+//     var child = list.lastChild;
+//     if (child != null && child.style.color != "red") {
+//       // добавление атрибута style="color:red" для выбранного элемента.
+//       child.setAttribute("style", "color:red;");
+//     }
+//     else if(child.style.color = "red") {
+//       //alert("loh");
+//       child.setAttribute("style", "color:black;");
+//     }
+//   }
+//
+//
+//   var node = null;
+//
+//   document.getElementById("SelectNextNode").onclick = function () {
+//     resetColor();
+//     if (node == null) {
+//       var list = document.getElementById("list_5");
+//       node = list.firstChild;
+//       node.setAttribute("style", "color:red");
+//       return;
+//     }
+//     // Получение следующего элемента, которые в дереве находиться на одном уровне.
+//     node = node.nextSibling;
+//     if (node != null) {
+//       node.setAttribute("style", "color:red");
+//       //alert(node.firstChild.nodeType);
+//     }
+//   }
+//
+//   document.getElementById("SelectPrevNode").onclick = function () {
+//     resetColor();
+//     if (node == null) {
+//       var list = document.getElementById("list_5");
+//       node = list.lastChild;
+//       node.setAttribute("style", "color:blue");
+//       return;
+//     }
+//     // Получение предыдущего элемента, которые в дереве находиться на одном уровне.
+//     node = node.previousSibling;
+//     if (node != null) {
+//       node.setAttribute("style", "color:blue");
+//     }
+//   }
+//
+//   function resetColor() {
+//     var liList = document.getElementsByTagName("li");
+//     for (var i = 0; i < liList.length; i++) {
+//       liList[i].setAttribute("style", "color:black");
+//     }
+//   }
+//
+//
+//   document.getElementById("createNewChild").onclick = function () {
+//     var list = document.getElementById("list_5");
+//     // создание элемента li
+//     var item = document.createElement("li");
+//     item.innerHTML = "<H1>ХУЙ</H1>";
+//     // добавление созданного элемента в конце.
+//     list.appendChild(item);
+//   }
+//
+//   document.getElementById("removeLastChild").onclick = function () {
+//     var list = document.getElementById("list_5");
+//     var item = list.lastChild;
+//     if (item != null) {
+//       // удаление выбранного элемента из дерева.
+//       list.removeChild(item);
+//     }
+//   }
+//
+//
+//   document.getElementById("createNewChildAtStart").onclick = function () {
+//     var list = document.getElementById("list_5");
+//     var item = document.createElement("li");
+//     item.innerHTML = "ХУЙ";
+//     if (list.firstChild != null) {
+//       // вставка элемента перед указанным элементом.
+//       list.insertBefore(item, list.firstChild);
+//     }
+//   }
+//
+//
+//   document.getElementById("yellow").onclick = function () {
+//     var liList = document.getElementsByTagName("li");
+//     for (var i = 0; i < liList.length; i++) {
+//       liList[i].setAttribute("style", "color:yellow");
+//     }
+//   }
+//
+//
 
-  document.getElementById("SelectFirstChild").onclick = function () {
-    var list = document.getElementById("list_5");
-    // Получение первого дочернего элемента в списке.
-    var child = list.firstChild;
-      if (child != null) {
-        var texColor = child.style.color ? child.style.color : 'black';
-        // добавление атрибута style="color:red" для выбранного элемента.
-        //  child.setAttribute("style", "color:black;");
+  // document.getElementById("sortChildren").onclick =
+function sortChildren(e) {
+  if(typeof  e == "string") e = document.getElementById(e);
 
-         //alert(texColor);
-        switch (texColor) {
-          case "black": child.setAttribute("style", "color:purple;");
-            break;
-          case "purple": child.setAttribute("style", "color:blue;");
-            break;
-          case "blue": child.setAttribute("style", "color:green;");
-            break;
-          case "green": child.setAttribute("style", "color:red;");
-            break;
-          case "red": child.setAttribute("style", "color:yellow;");
-            break;
-          case "yellow": child.setAttribute("style", "color:grey;");
-            break;
-          case "grey": child.setAttribute("style", "color:black;");
-            break;
-        }
+  //преобразовываем дочерние элементы в массив
+  var children = [];
+  for (var x = e.firstChild; x != null; x = x.nextSibling)
+    if(x.nodeType == 1 /* Node.ELEMENT_nNODE*/) children.push(x);
 
-    }
-  };
+    //сортировка
+  children.sort(function (n, m) { //функция сравнения
+    var s = n.firstChild.data;        //текст узла n
+    var t = m.firstChild.data;        //текст узла m
+    if (s < t) return -1;             //n перед m
+    else if (s > t) return 1          //m перед n
+    else return  0;                   //n и m равны
+  });
 
-  document.getElementById("SelectLastChild").onclick = function () {
-    var list = document.getElementById("list_5");
-    // Получение первого дочернего элемента в списке.
-    var child = list.lastChild;
-    if (child != null && child.style.color != "red") {
-      // добавление атрибута style="color:red" для выбранного элемента.
-      child.setAttribute("style", "color:red;");
-    }
-    else if(child.style.color = "red") {
-      //alert("loh");
-      child.setAttribute("style", "color:black;");
-    }
-  }
-
-
-  var node = null;
-
-  document.getElementById("SelectNextNode").onclick = function () {
-    resetColor();
-    if (node == null) {
-      var list = document.getElementById("list_5");
-      node = list.firstChild;
-      node.setAttribute("style", "color:red");
-      return;
-    }
-    // Получение следующего элемента, которые в дереве находиться на одном уровне.
-    node = node.nextSibling;
-    if (node != null) {
-      node.setAttribute("style", "color:red");
-      //alert(node.firstChild.nodeType);
-    }
-  }
-
-  document.getElementById("SelectPrevNode").onclick = function () {
-    resetColor();
-    if (node == null) {
-      var list = document.getElementById("list_5");
-      node = list.lastChild;
-      node.setAttribute("style", "color:blue");
-      return;
-    }
-    // Получение предыдущего элемента, которые в дереве находиться на одном уровне.
-    node = node.previousSibling;
-    if (node != null) {
-      node.setAttribute("style", "color:blue");
-    }
-  }
-
-  function resetColor() {
-    var liList = document.getElementsByTagName("li");
-    for (var i = 0; i < liList.length; i++) {
-      liList[i].setAttribute("style", "color:black");
-    }
-  }
-
-
-  document.getElementById("createNewChild").onclick = function () {
-    var list = document.getElementById("list_5");
-    // создание элемента li
-    var item = document.createElement("li");
-    item.innerHTML = "<H1>ХУЙ</H1>";
-    // добавление созданного элемента в конце.
-    list.appendChild(item);
-  }
-
-  document.getElementById("removeLastChild").onclick = function () {
-    var list = document.getElementById("list_5");
-    var item = list.lastChild;
-    if (item != null) {
-      // удаление выбранного элемента из дерева.
-      list.removeChild(item);
-    }
-  }
-
-
-  document.getElementById("createNewChildAtStart").onclick = function () {
-    var list = document.getElementById("list_5");
-    var item = document.createElement("li");
-    item.innerHTML = "ХУЙ";
-    if (list.firstChild != null) {
-      // вставка элемента перед указанным элементом.
-      list.insertBefore(item, list.firstChild);
-    }
-  }
-
-
-  document.getElementById("yellow").onclick = function () {
-    var liList = document.getElementsByTagName("li");
-    for (var i = 0; i < liList.length; i++) {
-      liList[i].setAttribute("style", "color:yellow");
-    }
-  }
-
-
+  //добавляем дочерние элементы обратно в родительский узел
+  //когда добавляется узел, который уже существует в документе, он автоматически удаляется со
+  for (var i = 0; i < children.length; i++) e.appendChild(children[i]);
 }
 
-
-
+}
 
 
 
