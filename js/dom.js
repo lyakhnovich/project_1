31698734bdcbbdda9b796cd48430f24c6745fc20
Lyakhnovich German <lyakhnovich.german@gmail.com>
2020-08-27 17:24:05 +0300

    itvdn - ADVANCED - lesson 2 - DOM - сортировка

--- js/dom.js
+++ js/dom.js
@@ -204,24 +204,35 @@ window.onload = function () {
   var e = 'mylist';
   if(typeof  e == "string") e = document.getElementById(e);
 
-
+  //alert(e.innerHTML);
   //преобразовываем дочерние элементы в массив
-  var children = [];
-  for (var x = e.firstChild; x != null; x = x.nextSibling)
-    if(x.nodeType == 1 /* Node.ELEMENT_nNODE*/) children.push(x);
+  var children = [], i = 0;
 
+  for (var x = e.firstChild; x != null; x = x.nextSibling) {
+    if(x.nodeType == 1 /* Node.ELEMENT_nNODE*/) children.push(x);
+    // if(x.nodeType == 8 /* Node.ELEMENT_nNODE*/) alert('HUI');
+  }
     //сортировка
+    console.log('---------------');
+
   children.sort(function (n, m) { //функция сравнения
+    i++;
     var s = n.firstChild.data;        //текст узла n
     var t = m.firstChild.data;        //текст узла m
-    if (s < t) return -1;             //n перед m
+    console.log(n,m,s,t);
+    if (s < t) return -1;//n перед m
     else if (s > t) return 1          //m перед n
     else return  0;                   //n и m равны
   });
+    console.log('i:', i);
+    console.log('---------------');
 
   //добавляем дочерние элементы обратно в родительский узел
-  //когда добавляется узел, который уже существует в документе, он автоматически удаляется со
+  //когда добавляется узел, который уже существует в документе, он автоматически удаляется со своего старого места
   for (var i = 0; i < children.length; i++) e.appendChild(children[i]);
+
+  var myarray = ["one", "two", "three", "four"];
+   ///alert(myarray.sort());
 }
 
 }
