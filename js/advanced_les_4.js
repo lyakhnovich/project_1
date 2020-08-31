57cceeceb824ad1cc75b4df54e96b820c764f08c
Lyakhnovich German <lyakhnovich.german@gmail.com>
2020-08-31 12:28:58 +0300

    itvdn - ADVANCED - lesson 4

--- js/advanced_les_4.js
+++ js/advanced_les_4.js
@@ -1,4 +1,24 @@
 // setTimeout - планирует один запуск указанной функции через определенное количество миллисекунд
-setTimeout(function showMessage() {
-  alert("hello");
-}, 5000); // запуск через 5 сек
+
+// setTimeout(showMessage, 4000);// запуск через 4 сек
+//
+// function showMessage() {
+//   alert("Timeout 1");
+// }
+//
+//
+// setTimeout(function showMessage() {
+//   alert("Timeout 2");
+// }, 10000); // запуск через 10 сек
+//
+//
+
+let cnt = 0;
+function Count() {
+  cnt++;
+  let item = document.createElement('p');
+  item.innerHTML = cnt;
+  document.getElementById("output").appendChild(item);
+}
+//setInterval - планирование запуска указанной функции с заданным интервалом
+setInterval(Count, 1000);
