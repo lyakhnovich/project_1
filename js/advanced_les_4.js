04696f9a105f1aae41b02a4ab0e5771f26e1537c
Lyakhnovich German <lyakhnovich.german@gmail.com>
2020-09-02 15:09:49 +0300

    itvdn - ADVANCED - lesson 4

--- js/advanced_les_4.js
+++ js/advanced_les_4.js
@@ -1,6 +1,6 @@
-////--------------  01 Timers  --------------
+// //--------------  01 Timers  --------------
 
-//// 01.1 setTimeout. Планирует один запуск указанной функции через определенное количество миллисекунд
+// // 01.1 setTimeout. Планирует один запуск указанной функции через определенное количество миллисекунд
 
 // setTimeout(showMessage, 4000);// запуск через 4 сек
 //
@@ -12,7 +12,7 @@
 // }, 10000); // запуск через 10 сек
 
 
-//// 01.2 setInterval
+// // 01.2 setInterval
 
 // let cnt = 0;
 // function Count() {
@@ -24,7 +24,7 @@
 // //setInterval - планирование запуска указанной функции с заданным интервалом
 // setInterval(Count, 1000);
 
-//// 01.3 clearTimeout
+// // 01.3 clearTimeout
 
 // timer = window.setTimeout(showMessage, 5000);
 //
@@ -36,7 +36,7 @@
 //   clearTimeout(timer);
 // }
 
-//// 01.4 clearInterval
+// // 01.4 clearInterval
 
 // window.onload = function (){
 //
@@ -61,8 +61,8 @@
 //     return document.getElementById(id);
 //   }
 
-////--------------  02 Location  --------------
-//// 02.1 Location
+// //--------------  02 Location  --------------
+// // 02.1 Location
 //
 // document.write('location = ' + document.location + '<br/>'); //весь url
 // document.write('protocol = ' + document.location.protocol + '<br/>'); //протокол
@@ -110,15 +110,15 @@
 // };
 
 
-//--------------  03 Navigator  --------------
-// 03.1 Сведенья о браузере
-
-// объект Navigator поддерживает пять свойств, по которым можно определить версию браузера
-// appName - название веб браузера
-// appVersion - номер версии или другая информация о браузере
-// userAgent - строка, которую браузер посылает в HTTP заголовке USER-AGENT
-// appCodeName - кодовое имя браузера
-// platform - аппаратная платформа, на которой работает браузер
+// //--------------  03 Navigator  --------------
+// //03.1 Сведенья о браузере
+//
+// // объект Navigator поддерживает пять свойств, по которым можно определить версию браузера
+// // appName - название веб браузера
+// // appVersion - номер версии или другая информация о браузере
+// // userAgent - строка, которую браузер посылает в HTTP заголовке USER-AGENT
+// // appCodeName - кодовое имя браузера
+// // platform - аппаратная платформа, на которой работает браузер
 
 // var browserInfo = 'Сведениья о браузере \n';
 // for (var propname in navigator) {
@@ -127,33 +127,59 @@
 // alert(browserInfo);
 
 
-//--------------  04 Windows  --------------
-// 04.1 Window Geometry. Определение параметров окна
+// //--------------  04 Windows  --------------
+// // 04.1 Window Geometry. Определение параметров окна
+//
+// let geometry = {};
+//
+// if (window.screenLeft || window.screenLeft == 0) {                                                    // для IE
+//   geometry.getWindowX = function () { return window.screenLeft; };
+//   geometry.getWindowY = function () { return window.screenTop; };
+// }
+// else if (window.screenX || window.screenX == 0) {                                                     // для Firefox
+//   geometry.getWindowY = function () { return window.screenX; };
+//   geometry.getWindowY = function () { return window.screenY; };
+// }
+//
+// if (window.innerWidth) {
+//   geometry.gitViewportWidth = function () { return window.innerWidth; };
+//   geometry.gitViewportHeight = function () { return window.innerHeight; };
+// }
+// else if (document.documentElement && document.documentElement.clientWidth) {                          // для IE6
+//   geometry.gitViewportWidth = function () { return document.documentElement.clientWidth; };
+//   geometry.gitViewportHeight = function () { return document.documentElement.clientHeight; };
+// }
+//
+// document.write("X position " + geometry.getWindowX() + "<br/>");
+// document.write("Y position " + geometry.getWindowX() + "<br/>");
+// document.write("Height " + geometry.gitViewportHeight() + "<br/>");
+// document.write("Width " + geometry.gitViewportWidth() + "<br/>");
 
-let geometry = {};
+// // 04.2 Screen. Определение высоты и ширины экрана
+//
+// window.onload = function () {
+//   alert("Width " + screen.width + "; Height = " + screen.height);
+// }
 
-if (window.screenLeft || window.screenLeft == 0) {                                                    // для IE
-  geometry.getWindowX = function () { return window.screenLeft; };
-  geometry.getWindowY = function () { return window.screenTop; };
-}
-else if (window.screenX || window.screenX == 0) {                                                     // для Firefox
-  geometry.getWindowY = function () { return window.screenX; };
-  geometry.getWindowY = function () { return window.screenY; };
+// 04.03 WindowOpen
+
+window.onload = function () {
+
+  document.getElementById("openButton").onclick = openButtonHandler;
+  document.getElementById("closeButton").onclick = closeButtonHandler;
+
+  var w;
+
+  function openButtonHandler() {
+    w = window.open("window.html", "Window Name", "width=400, height=400, status=yes, resizable=no")
+  }
+
+  function closeButtonHandler() {
+    w.close();
+  }
 }
 
-if (window.innerWidth) {
-  geometry.gitViewportWidth = function () { return window.innerWidth; };
-  geometry.gitViewportHeight = function () { return window.innerHeight; };
-}
-else if (document.documentElement && document.documentElement.clientWidth) {                          // для IE6
-  geometry.gitViewportWidth = function () { return document.documentElement.clientWidth; };
-  geometry.gitViewportHeight = function () { return document.documentElement.clientHeight; };
-}
 
-document.write("X position " + geometry.getWindowX() + "<br/>");
-document.write("Y position " + geometry.getWindowX() + "<br/>");
-document.write("Height " + geometry.gitViewportHeight() + "<br/>");
-document.write("Width " + geometry.gitViewportWidth() + "<br/>");
 
 
 
