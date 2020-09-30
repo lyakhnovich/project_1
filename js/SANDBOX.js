7e3ded565b27e2415912592a056c7fc9c20f87d5
Lyakhnovich German <lyakhnovich.german@gmail.com>
2020-09-30 15:52:27 +0300

    itvdn - Payment Page

--- js/SANDBOX.js
+++ js/SANDBOX.js
@@ -1,9 +1,3 @@
-// window.onload = function() {
-//   document.getElementById("mmyyyy").value = "221990";
-// };
-
-
-//------------------Функциональность поля PAN------------------//
 let input_keyup,
   input_click,
   caret_position = 0,
@@ -13,54 +7,68 @@ let input_keyup,
   left_symbol,
   right_symbol;
 
-document.addEventListener('keydown', function (event) {
-  if(event.code === 'Delete' || event.code == 'Backspace') {
-    key_name = event.code;
-  }
-});
 
-document.getElementById("pan").addEventListener("keydown", function () {
-  input_keyup = this.selectionStart;
-});
-
-document.getElementById("pan").addEventListener("keyup", function () {
-  input_keyup = this.selectionStart;
-});
-
-document.getElementById("pan").addEventListener("click", function () {
-  input_click = this.selectionStart;
-});
-
-class Position {
-  handleEvent(event) {
-    switch(event.type) {
-      case 'click':
-        caret_position = input_click;
-        last_symbol = document.getElementById("pan").value.substr(0, caret_position-1).slice(-1);
-        break;
-      case 'keyup':
-        caret_position = input_keyup;
-        last_symbol = document.getElementById("pan").value.substr(0, caret_position-1).slice(-1);
-        break;
-      case 'keydown':
-        caret_position = input_keyup;
-        last_symbol = document.getElementById("pan").value.substr(0, caret_position-1).slice(-1);
-        break;
+function key_info(id_elem) {
+  document.addEventListener('keydown', function (event) {
+    if(event.code === 'Delete' || event.code == 'Backspace') {
+      key_name = event.code;
     }
-      left_symbol = document.getElementById("pan").value[caret_position-1];
-      right_symbol = document.getElementById("pan").value[caret_position];
+  });
+
+  document.getElementById(id_elem).addEventListener("keydown", function () {
+    input_keyup = this.selectionStart;
+  });
+
+  document.getElementById(id_elem).addEventListener("keyup", function () {
+    input_keyup = this.selectionStart;
+  });
+
+  document.getElementById(id_elem).addEventListener("click", function () {
+    input_click = this.selectionStart;
+    console.log(input_click);
+  });
+
+  class Position {
+    handleEvent(event) {
+      switch(event.type) {
+        case 'click':
+          caret_position = input_click;
+          last_symbol = document.getElementById(id_elem).value.substr(0, caret_position-1).slice(-1);
+          break;
+        case 'keyup':
+          caret_position = input_keyup;
+          last_symbol = document.getElementById(id_elem).value.substr(0, caret_position-1).slice(-1);
+          break;
+        case 'keydown':
+          caret_position = input_keyup;
+          last_symbol = document.getElementById(id_elem).value.substr(0, caret_position-1).slice(-1);
+          break;
+      }
+      left_symbol = document.getElementById(id_elem).value[caret_position-1];
+      right_symbol = document.getElementById(id_elem).value[caret_position];
+    }
   }
+
+  let position = new Position();
+
+  document.getElementById(id_elem).addEventListener('click', position);
+  document.getElementById(id_elem).addEventListener('keyup', position);
+  document.getElementById(id_elem).addEventListener('keydown', position);
 }
 
-let position = new Position();
 
-document.getElementById("pan").addEventListener('click', position);
-document.getElementById("pan").addEventListener('keyup', position);
-document.getElementById("pan").addEventListener('keydown', position);
 
+
+
+
+
+
+//------------------Функциональность поля PAN------------------//
 document.getElementById("pan").oninput = main_pan;
 
 function main_pan() {
+  key_info("pan");
+
   let x = document.getElementById("pan").value;
   let arr = [];
 
@@ -80,12 +88,14 @@ function main_pan() {
   }
 
   x = onlyNum(x, "pan");
-  x = cut19(x);
+  x = cut_num(x, 19);
   if (!luhnAlgorithm(x)) {
-    console.log("PIZDEC");
-  };
-  x = refresh_input(x);
+    console.log("non valid Luhn");
+  }
+  x = refresh_input(x, " ", 4);
   document.getElementById("pan").value = x;
+
+
 
   if(key_name == "Backspace" && left_symbol == " ") {
     setCaretPosition(document.getElementById("pan"), caret_position-2);
@@ -103,18 +113,36 @@ function main_pan() {
   else if (key_name == "Delete" && right_symbol != " "){
     setCaretPosition(document.getElementById("pan"), caret_position);
   }
+
+
+
   key_name = "";
+
+  console.log(" |input_keyup " + input_keyup +
+    " |input_click " + input_click +
+    " |caret_position " + caret_position +
+    " |key_name " + key_name +
+    " |last_symbol " + last_symbol +
+    " |left_symbol " + left_symbol +
+    " |right_symbol " + right_symbol
+  );
 }
 
 
-function refresh_input(txt) {
+function refresh_input(txt, separator, interval) {
   let result = "";
-  let cnt = 3;
+  let cnt = interval-1;
 
   for (let i = 0; txt.length > i; i++) {
     if (i == cnt){
-      result += txt[i].concat(" ");
-      cnt += 4;
+      result += txt[i].concat(separator);
+
+      if (separator == " ") {
+        cnt += 4;
+      }
+      else if (separator == "/") {
+        cnt += 5;
+      }
     }
     else {
       result += txt[i];
@@ -131,10 +159,10 @@ function onlyNum(txt, id_elem) {
   return result;
 }
 
-function cut19(txt) {
+function cut_num(txt, num) {
   let result;
-  if (txt.length > 19) {
-    result = txt.substr(0, 19);
+  if (txt.length > num) {
+    result = txt.substr(0, num);
   }
   else result = txt;
   return result;
@@ -177,51 +205,63 @@ function luhnAlgorithm(digits) {
 document.getElementById("mmyyyy").oninput = main_mmyyyy;
 
 
-let mm = "",
-  yyyy = "";
-
 function main_mmyyyy() {
-  let temp = document.getElementById("mmyyyy").value;
-  let y = document.getElementById("mmyyyy").value;
+  key_info("mmyyyy");
 
-  y = onlyNum(y, "mmyyyy");
-  y = cut_6(y);
-  // console.log("START " + y);
+  let x = document.getElementById("mmyyyy").value;
+  let arr = [];
 
-
-  for (let i = 0; y.length > i; i++) {
-    console.log("ты дебил? " + y.length + " " + y);
-    if(y.length <= 2) {
-      if(mm > 12) {
-        mm = 12;
-        break;
-      }
-      mm = y;
-    }
-    else {
-      yyyy = y.slice(2, );
-    }
+  for (let i = 0; i < x.length ; i++) {
+    arr.push(x[i]);
   }
 
-
-  //console.log("mm: " + mm + " yyyy: " + yyyy + " (temp: " + temp + ")");
-  document.getElementById("mmyyyy").value = mm + "/" + yyyy;
-}
-
-
-
-
-
-function cut_6(mm_yyyy) {
-  let result;
-  if (mm_yyyy.length > 6) {
-    result = mm_yyyy.substr(0, 6);
+  if(key_name == "Backspace" && left_symbol == "/") {
+    arr.splice(caret_position-2, 1);
+    arr = arr.join("");
+    x = arr.toString();
   }
-  else result = mm_yyyy;
-  return result;
+  if(key_name == "Delete" && right_symbol == "/") {
+    arr.splice(caret_position, 1);
+    arr = arr.join("");
+    x = arr.toString();
+  }
+
+  x = onlyNum(x, "mmyyyy");
+  x = cut_num(x, 6);
+
+  x = refresh_input(x, "/", 2);
+  document.getElementById("mmyyyy").value = x;
+
+
+
+  if(key_name == "Backspace" && left_symbol == "/") {
+    setCaretPosition(document.getElementById("mmyyyy"), caret_position-2);
+  }
+  else if(key_name == "Backspace" && last_symbol != "/") {
+    setCaretPosition(document.getElementById("mmyyyy"), caret_position-1);
+  }
+  else if (key_name == "Backspace" && last_symbol == "/") {
+    setCaretPosition(document.getElementById("mmyyyy"), caret_position-2);
+  }
+
+  if(key_name == "Delete" && right_symbol == "/") {
+    setCaretPosition(document.getElementById("mmyyyy"), caret_position+1);
+  }
+  else if (key_name == "Delete" && right_symbol != "/"){
+    setCaretPosition(document.getElementById("mmyyyy"), caret_position);
+  }
+
+  key_name = "";
+
+  console.log(" |input_keyup " + input_keyup +
+    " |input_click " + input_click +
+    " |caret_position " + caret_position +
+    " |key_name " + key_name +
+    " |last_symbol " + last_symbol +
+    " |left_symbol " + left_symbol +
+    " |right_symbol " + right_symbol
+  );
 }
-
-
 
 
 
