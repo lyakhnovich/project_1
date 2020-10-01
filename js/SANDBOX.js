e3624a99ac526ed8761fd83bc2fc1f2cb0ec999e
Lyakhnovich German <lyakhnovich.german@gmail.com>
2020-10-01 16:35:34 +0300

    itvdn - Payment Page

--- js/SANDBOX.js
+++ js/SANDBOX.js
@@ -3,9 +3,84 @@ let input_keyup,
   caret_position = 0,
   key_name,
   last_symbol,
-
   left_symbol,
   right_symbol;
+
+
+//------------------Функциональность поля PAN------------------//
+document.getElementById("pan").oninput = function () {
+  main("pan", " ");
+};
+
+//------------------Функциональность поля MMYYYY------------------//
+document.getElementById("mmyyyy").oninput = function () {
+  main("mmyyyy", "/");
+};
+
+function main(id_elem, separator) {
+  key_info(id_elem);
+
+  let x = document.getElementById(id_elem).value;
+  let arr = [];
+
+  for (let i = 0; i < x.length ; i++) {
+    arr.push(x[i]);
+  }
+
+  if(key_name === "Backspace" && left_symbol === separator) {
+    arr.splice(caret_position-2, 1);
+    arr = arr.join("");
+    x = arr.toString();
+  }
+  if(key_name === "Delete" && right_symbol === separator) {
+    arr.splice(caret_position, 1);
+    arr = arr.join("");
+    x = arr.toString();
+  }
+
+  if(id_elem === "mmyyyy") {
+    x = onlyNum(x, id_elem);
+    x = cut_num(x, 6);
+    x = refresh_input(x, separator, 2);
+  }
+  else if (id_elem === "pan") {
+    x = onlyNum(x, "pan");
+    x = cut_num(x, 19);
+    if (!luhnAlgorithm(x)) {
+      console.log("non valid Luhn");
+    }
+    x = refresh_input(x, " ", 4);
+  }
+  document.getElementById(id_elem).value = x;
+
+  if(key_name === "Backspace" && left_symbol === separator) {
+    setCaretPosition(document.getElementById(id_elem), caret_position-2);
+  }
+  else if(key_name === "Backspace" && last_symbol !== separator) {
+    setCaretPosition(document.getElementById(id_elem), caret_position-1);
+  }
+  else if (key_name === "Backspace" && last_symbol === separator) {
+    setCaretPosition(document.getElementById(id_elem), caret_position-2);
+  }
+
+  if(key_name === "Delete" && right_symbol === separator) {
+    setCaretPosition(document.getElementById(id_elem), caret_position+1);
+  }
+  else if (key_name === "Delete" && right_symbol !== separator){
+    setCaretPosition(document.getElementById(id_elem), caret_position);
+  }
+
+  key_name = "";
+
+  console.log(" |input_keyup " + input_keyup +
+    " |input_click " + input_click +
+    " |caret_position " + caret_position +
+    " |key_name " + key_name +
+    " |last_symbol " + last_symbol +
+    " |left_symbol " + left_symbol +
+    " |right_symbol " + right_symbol
+  );
+}
 
 
 function key_info(id_elem) {
@@ -56,77 +131,6 @@ function key_info(id_elem) {
   document.getElementById(id_elem).addEventListener('keydown', position);
 }
 
-
-
-
-
-
-//------------------Функциональность поля PAN------------------//
-document.getElementById("pan").oninput = main_pan;
-
-function main_pan() {
-  key_info("pan");
-
-  let x = document.getElementById("pan").value;
-  let arr = [];
-
-  for (let i = 0; i < x.length ; i++) {
-    arr.push(x[i]);
-  }
-
-  if(key_name === "Backspace" && left_symbol === " ") {
-    arr.splice(caret_position-2, 1);
-    arr = arr.join("");
-    x = arr.toString();
-  }
-  if(key_name === "Delete" && right_symbol === " ") {
-    arr.splice(caret_position, 1);
-    arr = arr.join("");
-    x = arr.toString();
-  }
-
-  x = onlyNum(x, "pan");
-  x = cut_num(x, 19);
-  if (!luhnAlgorithm(x)) {
-    console.log("non valid Luhn");
-  }
-  x = refresh_input(x, " ", 4);
-  document.getElementById("pan").value = x;
-
-
-
-  if(key_name === "Backspace" && left_symbol === " ") {
-    setCaretPosition(document.getElementById("pan"), caret_position-2);
-  }
-  else if(key_name === "Backspace" && last_symbol !== " ") {
-    setCaretPosition(document.getElementById("pan"), caret_position-1);
-  }
-  else if (key_name === "Backspace" && last_symbol === " ") {
-    setCaretPosition(document.getElementById("pan"), caret_position-2);
-  }
-
-  if(key_name === "Delete" && right_symbol === " ") {
-    setCaretPosition(document.getElementById("pan"), caret_position+1);
-  }
-  else if (key_name === "Delete" && right_symbol !== " "){
-    setCaretPosition(document.getElementById("pan"), caret_position);
-  }
-
-
-
-  key_name = "";
-
-  console.log(" |input_keyup " + input_keyup +
-    " |input_click " + input_click +
-    " |caret_position " + caret_position +
-    " |key_name " + key_name +
-    " |last_symbol " + last_symbol +
-    " |left_symbol " + left_symbol +
-    " |right_symbol " + right_symbol
-  );
-}
-
-
 function refresh_input(txt, separator, interval) {
   let result = "";
   let cnt = interval-1;
@@ -146,7 +150,6 @@ function refresh_input(txt, separator, interval) {
       result += txt[i];
     }
   }
-
   return result;
 }
 
@@ -193,71 +196,4 @@ function luhnAlgorithm(digits) {
 }
 
 
-
-
-
-
-
-//------------------Функциональность поля MMYYYY------------------//
-
-document.getElementById("mmyyyy").oninput = main_mmyyyy;
-
-
-function main_mmyyyy() {
-  key_info("mmyyyy");
-
-  let x = document.getElementById("mmyyyy").value;
-  let arr = [];
-
-  for (let i = 0; i < x.length ; i++) {
-    arr.push(x[i]);
-  }
-
-  if(key_name === "Backspace" && left_symbol === "/") {
-    arr.splice(caret_position-2, 1);
-    arr = arr.join("");
-    x = arr.toString();
-  }
-  if(key_name === "Delete" && right_symbol === "/") {
-    arr.splice(caret_position, 1);
-    arr = arr.join("");
-    x = arr.toString();
-  }
-
-  x = onlyNum(x, "mmyyyy");
-  x = cut_num(x, 6);
-
-  x = refresh_input(x, "/", 2);
-  document.getElementById("mmyyyy").value = x;
-
-
-
-  if(key_name === "Backspace" && left_symbol === "/") {
-    setCaretPosition(document.getElementById("mmyyyy"), caret_position-2);
-  }
-  else if(key_name === "Backspace" && last_symbol !== "/") {
-    setCaretPosition(document.getElementById("mmyyyy"), caret_position-1);
-  }
-  else if (key_name === "Backspace" && last_symbol === "/") {
-    setCaretPosition(document.getElementById("mmyyyy"), caret_position-2);
-  }
-
-  if(key_name === "Delete" && right_symbol === "/") {
-    setCaretPosition(document.getElementById("mmyyyy"), caret_position+1);
-  }
-  else if (key_name === "Delete" && right_symbol !== "/"){
-    setCaretPosition(document.getElementById("mmyyyy"), caret_position);
-  }
-
-  key_name = "";
-
-  console.log(" |input_keyup " + input_keyup +
-    " |input_click " + input_click +
-    " |caret_position " + caret_position +
-    " |key_name " + key_name +
-    " |last_symbol " + last_symbol +
-    " |left_symbol " + left_symbol +
-    " |right_symbol " + right_symbol
-  );
-}
 
