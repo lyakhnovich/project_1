9ca33c882f64edca34a4c177e1897f1765e73dfa
Lyakhnovich German <lyakhnovich.german@gmail.com>
2020-10-07 11:53:50 +0300

    itvdn - jquery

---
+++ js/jquery_les_2.js
@@ -0,0 +1,36 @@
+//----------------------------Even Odd--------------------------------//
+
+// Even - выбор четных
+// Odd - выбор нечетных
+
+// $(function () {
+//   // $("p:even").css("background-color", "red");
+//   $("p:odd").css("background-color", "red");
+// });
+
+
+//----------------------------First Last--------------------------------//
+
+// $(function () {
+//   $("li:first").css("color", "red");
+//   $("li:last").css("color", "red");
+// });
+
+
+//----------------------------First-child Last-child--------------------------------//
+
+$(function () {
+  $("ul li:first-child").css("color", "red");
+  // $("ul li:last-child").css("color", "red");
+});
+
+
+//----------------------------Only-child--------------------------------//
+
+
+
+
+
+
+
+
