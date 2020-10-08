ba2de46eccf5a3071adc6da8ae2e0d455dcb9c76
Lyakhnovich German <lyakhnovich.german@gmail.com>
2020-10-08 09:53:27 +0300

    itvdn - jquery

--- js/jquery_les_2.js
+++ js/jquery_les_2.js
@@ -1,3 +1,4 @@
+//  ФИЛЬТРЫ В JQUERY
 //----------------------------Even Odd--------------------------------//
 
 // Even - выбор четных
@@ -58,12 +59,121 @@
 // :gt(индекс) - все элементы, индекс котрых больше указанного
 // :lt(индекс) - все элементы, индекс котрых меньше указанного
 
-$(function () {
-  // $("p").css("background-color", "#4dea4d");
-  // $("p:eq(3)").css("background-color", "#4dea4d");
-  // $("p:gt(4)").css("background-color", "#4dea4d");
-  $("p:lt(4)").css("background-color", "#4dea4d");
-});
+// $(function () {
+//   // $("p").css("background-color", "#4dea4d");
+//   // $("p:eq(3)").css("background-color", "#4dea4d");
+//   // $("p:gt(4)").css("background-color", "#4dea4d");
+//   $("p:lt(4)").css("background-color", "#4dea4d");
+// });
+
+
+//----------------------------Eq example--------------------------------//
+
+// let i = 0;
+//
+// $(function () {
+//   let timer = setInterval(choose, 1500);
+// });
+//
+// function choose() {
+//   i++;
+//
+//   $("li:eq(" + (i - 1) + ")")
+//     .css("background-color", "white");
+//
+//   $("li:eq(" + i + ")")
+//     .css("background-color", "yellow");
+// }
+
+
+//----------------------------Not--------------------------------//
+
+// $(function () {
+//   $("li:not(li:eq(3))").css("background-color", "red")
+// })
+
+
+//----------------------------Header--------------------------------//
+
+// $(function () {
+//   // $("h1, h2, h3, h4, h5, h6").css("background-color", "4dea4d")
+//   $(":header").css("background-color", "#4dea4d");
+//   // $( ":header" ).css({ background: "#ccc", color: "blue" });
+// });
+
+
+//----------------------------Animated--------------------------------//
+
+// $(function () {
+//   $(".last").hide(3000);
+//   $(":animated").css("background", "red")
+// })
+
+
+
+
+
+//----------------------------ФИЛЬТРЫ КОНТЕНТА--------------------------------//
+
+//----------------------------Contains--------------------------------//
+
+// $(function () {
+//   // $("p:contains('JavaScript')").css("background-color", "red");
+//   $("p:contains('HTML')").css("background-color", "red");
+// })
+
+// :empty
+// :parent
+// :has
+
+
+//----------------------------ФИЛЬТРЫ ФОРМ--------------------------------//
+// :input
+// :text
+// :password
+// :radio
+// :checkbox
+// :submit
+// :image
+// :reset
+// :button
+// :file
+
+
+//  МАНИПУЛЯЦИЯ СВОЙСТВАМИ, АТРИБУТАМИ И СТИЛЯМИ
+
+//----------------------------attr()--------------------------------//
+
+$('div:not(.func_div)').attr({
+                style: "color:red; font-size:22pt;",
+                title: "jquery attr test"
+              });
+
+
+$('.func_div').attr(
+                    "style", CalculateValue()
+                    );
+
+function CalculateValue() {
+  return "color:blue";
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
