3a361c286944e9137708fb5fa00744c6853afdbd
Lyakhnovich German <lyakhnovich.german@gmail.com>
2020-10-16 16:43:12 +0300

    itvdn - jquery

--- js/jquery_les_3.js
+++ js/jquery_les_3.js
@@ -14,26 +14,72 @@
 // });
 
 //----------------------------Is()--------------------------------//
+// $(function () {
+//   $("div").on('click', function () {
+//     if($(this).is(":first-child")) {
+//       $('p').text("гэта первый div");
+//     }
+//     else if ($(this).is(".blue, .red")) {
+//       $('p').text("гэта красный или синий div");
+//     }
+//     else if ($(this).is(":contains('Test')")) {
+//       $('p').text("содержит текст Test");
+//     }
+//     else {
+//       $('p').text("нэма нехуя");
+//     }
+//   })
+// });
 
+//----------------------------Filter()--------------------------------//
+// $(function () {
+//   let paragraphs = $('p');
+//   paragraphs.filter(".test").css("background-color", "red");
+//   paragraphs.filter(".odd").css("background-color", "green");
+// });
+
+//----------------------------Filter(function)--------------------------------//
+// $(function () {
+//
+//   $("p").filter(function (index) {
+//     if ($(this).attr("align") && index ==0) {
+//       return true;
+//     }
+//   }).css("background-color", "red");
+//
+// });
+
+//----------------------------Slice()--------------------------------//
 $(function () {
-  $("div").on('click', function () {
-    if($(this).is(":first-child")) {
-      $('p').text("гэта первый div");
-    }
-    else if ($(this).is(".blue, .red")) {
-      $('p').text("гэта красный или синий div");
-    }
-    else if ($(this).is(":contains('Test')")) {
-      $('p').text("содержит текст Test");
-    }
-    else {
-      $('p').text("нэма нехуя");
-    }
-  })
+  $('#btn1').click(function () {
+    $('p').css("background-color", "green");
+    $('p')
+      .slice(0, 3)
+      .css("background-color", "red");
+  });
+
+  $('#btn2').click(function () {
+    $('p').css("background-color", "green");
+    $('p')
+      .slice(2, 4)
+      .css("background-color", "red");
+  });
+
+  $('#btn3').click(function () {
+    $('p').css("background-color", "green");
+    $('p')
+      .slice(3)
+      .css("background-color", "red");
+  });
+
+  $('#btn4').click(function () {
+    $('p').css("background-color", "green");
+    $('p')
+      .slice(3, -3)
+      .css("background-color", "red");
+  });
+
 });
-
-
-
 
 
 
