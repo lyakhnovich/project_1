05f20ebf7e80f4ca2c4ccbfdabbcec660f35437f
Lyakhnovich German <lyakhnovich.german@gmail.com>
2020-10-21 09:39:19 +0300

    itvdn - jquery

---
+++ js/registration.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+
+  $('.input_enter').on('click', function () {
+    $('.enter').toggleClass("toggleClass")
+  });
+
+  $('.input_registration').on('click', function () {
+    $('.registration').toggleClass("toggleClass")
+  })
+
+
+
+});
