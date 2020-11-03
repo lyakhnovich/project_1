c22338820dcfe185792e87c87e54357614c327cc
Lyakhnovich German <lyakhnovich.german@gmail.com>
2020-11-03 15:45:27 +0300

    itvdn - jquery

---
+++ js/jquery_les_4.js
@@ -0,0 +1,49 @@
+//----------------------------событие ready--------------------------------//
+// ready - определяет функцию, которая будет запущена после полной загрузки DOM
+// 1
+// $(document).ready(function () {
+//   console.log('page loaded 1');
+// });
+//
+// // 2
+// $(document).ready(after_ready);
+// function after_ready() {
+//   console.log('page loaded 2');
+// }
+//
+// // 3 (сокращенный вариант с анонимной функцией)
+// $(function () {
+//   console.log('page loaded 3');
+// });
+
+
+//----------------------------click--------------------------------//
+
+$(function () {
+
+  $('p').on('click', function () {
+    console.log('click');
+  });
+
+  $('#button').on('click', function () {
+    console.log('click btn');
+    $('p').click();
+  });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
