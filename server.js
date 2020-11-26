debeb5a20c43efd607c08568dac32ea6ab55db5a
Lyakhnovich German <lyakhnovich.german@gmail.com>
2020-11-26 11:54:46 +0300

    itvdn - jquery - AJAX

--- server.js
+++ server.js
@@ -2,6 +2,23 @@ const http = require('http');
 const server =  http.createServer();
 
 server.on('request', (req, res) => {
-  res.end('server run blyat'); });
 
-server.listen(8000, () => console.log('server run blyat'));
+
+  function User(name) {
+    this.name = name;
+  }
+
+  User.prototype.hello = function (who) {
+    console.log('привет, ' + who.name);
+  };
+
+  let loh = new User('лох');
+  let pidr = new User('пидр');
+
+  pidr.hello(loh);
+
+
+
+  res.end('server run blyat!!!'); });
+
+server.listen(8000, () => console.log('server run blyat!'));
