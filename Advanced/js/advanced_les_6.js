e6c959bc2de27fca7c41ec9f97ef6efa3feb0fec
lyakhnovich <lyakhnovich.german@gmail.com>
2021-02-01 19:44:19 +0300

    itvdn

--- Advanced/js/advanced_les_6.js
+++ Advanced/js/advanced_les_6.js
@@ -1,3 +1,17 @@
 function validate() {
   alert('validate');
 }
+
+// window.onmousedown = function () {
+//   console.log('lol kek cheburek..');
+// };
+
+$('#test').onmousedown = function () {
+  console.log('pizda');
+};
+
+$('a').css({color: 'green'});
+
+
+
+
