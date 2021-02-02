96e26bfcd66aa7ba491f35e4e57455f431ebf3a7
lyakhnovich <lyakhnovich.german@gmail.com>
2021-02-02 13:33:29 +0300

    itvdn

--- Advanced/js/advanced_les_6.js
+++ Advanced/js/advanced_les_6.js
@@ -1,20 +1,48 @@
-function validate() {
-  alert('validate');
+// function validate() {
+//   alert('validate');
+// }
+//
+// $('#clickme').on('click', function () {
+//
+//   $('#clickme').css({color: 'red'});
+//   console.log('lol');
+// })
+//
+// $('a').on('click', function(e) {
+//   e.preventDefault();
+//   console.log('lol');
+//   return false;
+// });
+
+// window.onload =loadHandler;
+//
+// function loadHandler() {
+//   let p1 = document.getElementById('p1');
+//   let p2 = document.getElementById('p2');
+//
+//   p1.onclick = function () {
+//     console.log('hello');
+//   }
+//
+//   p2.onclick = clickHandler;
+// }
+//
+// function clickHandler() {
+//   console.log('hello 2!');
+// }
+
+
+window.onload = loadHandler2;
+
+function loadHandler2() {
+  let e = document.getElementById('input1');
+
+  e.onclick = function () {
+    this.value = 'clicked2!';
+    $('#input1').css({color: 'red'});
+  }
 }
 
-
-$('#clickme').on('click', function () {
-
-  $('#clickme').css({color: 'red'});
-  console.log('lol');
-})
-
-
-$('a').on('click', function(e) {
-  // e.preventDefault();
-  console.log('lol');
-  return false;
-});
 
 
 
